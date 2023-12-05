bcef08e5df4d8fb00802539e0e965334a05444d2
Ceasar2001 <ecleoceasar1@gmail.com>
2023-12-05 17:58:27 +0800

    new update

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
+import LandingView from '@/views/landingView.vue'
+import LocationView from '@/views/LocationView.vue'
+import MapView from '@/views/MapView.vue'
+import TripView from '@/views/TripView.vue'
+import axios from 'axios'
+import { isEqual } from 'fast-deep-equal';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,15 +15,56 @@ const router = createRouter({
       name: 'login',
       component: LoginView
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
+    {
+      path: '/landing',
+      name: 'landing',
+      component: LandingView
+    },
+    {
+      path: '/location',
+      name: 'location',
+      component: LocationView
+    },
+    {
+      path: '/map',
+      name: 'map',
+      component: MapView
+    },
+    {
+      path: '/trip',
+      name: 'trip',
+      component: TripView
+    }
   ]
 })
+
+router.beforeEach((to, from) => {
+  if(to.name === 'login'){
+    return true
+  }
+
+  if(!localStorage.getItem('token')){
+    return{
+      name: 'login'
+    }
+  }
+  
+ checkTokenAuthenticity()
+})
+
+const checkTokenAuthenticity = () => {
+  axios.get('http://127.0.0.1:8000/api/user', {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`
+    }
+  })
+  .then((response) => {})
+  .catch((error) => {
+    localStorage.removeItem('token')
+    router.push({
+      name: 'login'
+    })
+  })
+}
 
 export default router
